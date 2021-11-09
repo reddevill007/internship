05402ba8da86723d864edffb54eb932e2c93025a
Saurabh Pandey <74395114+reddevill007@users.noreply.github.com>
2021-11-09 23:01:24 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 var myForm = document.getElementById('myForm')
 var discount = document.getElementById('discount')
 var totalMoney = document.getElementById('totalMoney')
+var btn = document.getElementById('btn')
         
 myForm.addEventListener('submit', function(e){
     e.preventDefault()
     discount.classList.add('active')
     totalMoney.classList.add('active')
-    console.log("CSDFv")
+    btn.innerHTML = "Applied"
 })
